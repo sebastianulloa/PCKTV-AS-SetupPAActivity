10deefc4e569fff9b2bbc00b574401e659de2e6f
BlakeSM <blake.smethers@skyline.be>
2023-02-17 13:20:43 -0500

    Add project files.

---
+++ Setup PA Activity_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Setup PA Activity_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("Setup PA Activity_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("C2CAE59B-B314-4486-B3F4-C7858FE9E1AE")]
+[assembly: AssemblyVersion("1.0.0.0")]
